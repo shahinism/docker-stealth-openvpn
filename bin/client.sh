e4e892cc98ce186bb60473ff47740a86b72d6427
Shahin <me@5hah.in>
2020-04-19 17:32:11 +0430

    Push route config to clients.

--- bin/client.sh
+++ bin/client.sh
@@ -2,16 +2,21 @@
 
 set -e
 
-CLIENTS_DIR="./clients"
+# shellcheck source=./common.sh
+. "${BASH_SOURCE%/*}/common.sh"
+
 printf "Username: "
 read -r USERNAME
+
+CONFIG_DIR="./clients"
+CONFIG_PATH="$CLIENTS_DIR/$USERNAME".ovpn
 
 if [ "$USERNAME" = "" ]; then
     echo "Username was empty"
     exit 1
 fi
 
-mkdir -p $CLIENTS_DIR
-
+mkdir -p $CONFIG_DIR
+info "+ Start user generation"
 docker-compose run --rm openvpn easyrsa build-client-full "$USERNAME" nopass
-docker-compose run --rm openvpn ovpn_getclient "$USERNAME" > "$CLIENTS_DIR/$USERNAME".ovpn
+docker-compose run --rm openvpn ovpn_getclient "$USERNAME" >
--- bin/init.sh
+++ bin/init.sh
@@ -5,6 +5,9 @@ set -e
 # shellcheck source=./common.sh
 . "${BASH_SOURCE%/*}/common.sh"
 
+OVPN_CONF_DIR="./ovpn-data-store/"
+OVPN_CONF_PATH="${OVPN_CONF_DIR}/openvpn.conf"
+
 STUNNEL_CONF_PATH="./stunnel.conf"
 info "+ Initializing OpenVPN config..."
 
@@ -16,15 +19,19 @@ ADDRESS=${ADDRESS:-$PUBLIC_IP}
 docker-compose run --rm openvpn ovpn_genconfig  -C "AES-256-CBC" -a "SHA384" -u "tcp://$ADDRESS:443"
 docker-compose run --rm openvpn ovpn_initpki
 
+cat >> "$OVPN_CONF_PATH" <<EOF
+push "route ${ADDRESS} 255.255.255.255 net_gateway"
+EOF
+
 success "+ OpenVPN configuration succeeded"
 info "+ Initializing stunnel config for clients..."
 
-cat > "$STUNNEL_CONF_PATH" << _EOF_
+cat > "$STUNNEL_CONF_PATH" << EOF
 client = yes
 
 [stunnel.nl]
 accept = 127.0.0.1:41194
 connect = ${ADDRESS}:993
-_EOF_
+EOF
 
 success "+ Stunnel config is available at ${STUNNEL_CONF_PATH}"
